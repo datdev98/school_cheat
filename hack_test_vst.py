7da5b769b3b641f567a2bdf1bd46d3da0094076f
datdev <datdev0609@gmail.com>
2018-09-02 15:20:39 +0700

    format code

--- hack_test_vst.py
+++ hack_test_vst.py
@@ -16,13 +16,15 @@ def main():
         browser.visit('http://test.vst.edu.vn/thisinh/test/chuong/?edit=155')
         while (True):
             i = int(input('Enter number: '))
-            browser.find_by_css('html body div#body div.body-frame div#main-content.border.pattern.active div.grid-view-container table.grid-view tbody tr')[i-1].click()
+            browser.find_by_css(
+                'html body div#body div.body-frame div#main-content.border.pattern.active div.grid-view-container table.grid-view tbody tr')[i-1].click()
             result(browser)
             exit = input('Exit (y): ')
             if exit == 'y':
                 return
-            browser.visit('http://test.vst.edu.vn/thisinh/test/chuong/?edit=155')
+            browser.visit(
+                'http://test.vst.edu.vn/thisinh/test/chuong/?edit=155')
 
 
 if __name__ == "__main__":
-    main();
+    main()
--- sis_timetable.py
+++ sis_timetable.py
@@ -1,6 +1,6 @@
 from splinter import Browser
 
-browser = Browser('chrome')
+browser = Browser()
 
 
 def setup():
@@ -8,9 +8,12 @@ def setup():
 
 
 def login(username, password):
-    username_input = browser.find_by_xpath('//*[@id="cLogIn1_tb_cLogIn_User_I"]')[0]
-    password_input = browser.find_by_xpath('//*[@id="cLogIn1_tb_cLogIn_Pass_I"]')[0]
-    login_button = browser.find_by_xpath('//*[@id="cLogIn1_tb_cLogIn_Pass_I"]')[0]
+    username_input = browser.find_by_xpath(
+        '//*[@id="cLogIn1_tb_cLogIn_User_I"]')[0]
+    password_input = browser.find_by_xpath(
+        '//*[@id="cLogIn1_tb_cLogIn_Pass_I"]')[0]
+    login_button = browser.find_by_xpath(
+        '//*[@id="cLogIn1_tb_cLogIn_Pass_I"]')[0]
 
     username_input.fill(username)
     password_input.fill(password)
@@ -20,11 +23,13 @@ def login(username, password):
 def get_timetable(student_id):
     browser.visit('http://sis.hust.edu.vn/ModulePlans/Timetables.aspx')
 
-    student_id_input = browser.find_by_xpath('//*[@id="MainContent_Studentid_I"]')
+    student_id_input = browser.find_by_xpath(
+        '//*[@id="MainContent_Studentid_I"]')
     find_button = browser.find_by_xpath('//*[@id="MainContent_btFind_B"]')
 
     student_id_input.fill(student_id)
     find_button.click()
+
 
 if __name__ == '__main__':
     # setup()
